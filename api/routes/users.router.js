98490773dc00433d175087e9e87cbff68651d84c
David <j.david.prado1716@gmail.com>
2023-04-17 14:21:06 -0500

    integracion de node-postgres

--- api/routes/users.router.js
+++ api/routes/users.router.js
@@ -1,16 +1,74 @@
-const express=require('express');
+const express = require('express');
 
-const router=express.Router();
+const UserService = require('../service/user.service');
+const validatorHandler = require('../middlewares/validatorHandler');
+const { updateUserSchema, createUserSchema, getUserSchema } = require('../schemas/user.shema');
 
-router.get('/',(req,res)=>{
+const router = express.Router();
+// creamos un nuevo usuario.
+const service = new UserService();
 
-  const{limit,offset}=req.query;
-
-  if(limit&&offset){
-    res.json({limit,offset});
-  }else{
-    res.send('No hay parametros');
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await service.find();
+    res.json(users);
+  } catch (error) {
+    next(error);
   }
 });
 
-module.exports=router;
+router.get('/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const category = await service.findOne(id);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post('/',
+  validatorHandler(createUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newCategory = await service.create(body);
+      res.status(201).json(newCategory);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.patch('/:id',
+  validatorHandler(getUserSchema, 'params'),
+  validatorHandler(updateUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const category = await service.update(id, body);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete('/:id',
+  validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.status(201).json({id});
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+module.exports = router;
