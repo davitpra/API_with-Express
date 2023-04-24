687a5d5bc871891f36dbf19e955d910fa4777afb
David <j.david.prado1716@gmail.com>
2023-04-24 11:20:59 -0500

    realaciones muchos a muchos

--- api/schemas/product.schema.js
+++ api/schemas/product.schema.js
@@ -1,22 +1,28 @@
 const Joi=require('joi');
 
 //definimos el tipo de datos
-const id=Joi.string().uuid();
+const id=Joi.number().uuid();
 const name = Joi.string().min(3).max(15);
 const price = Joi.number().integer().min(10);
+const description = Joi.string().min(10);
 const image = Joi.string().uri();
+const categoryId=Joi.number().integer();
 
 //creamos los schemas y determinamos si son obligatorios.
 const createProductSchema=Joi.object({
-  name:name.required()
-  ,price:price.required(),
-  image: image.required()
+  name:name.required(),
+  price:price.required(),
+  description:description.required(),
+  image: image.required(),
+  categoryId:categoryId.required()
   });
 
 const updateProductSchema=Joi.object({
   name:name,
   price:price,
-  image:image
+  image:image,
+  description,
+  categoryId
   });
 
 const getProductSchema=Joi.object({
