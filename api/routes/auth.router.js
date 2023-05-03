7dab8e5b49f446b9b229716f6b0702267431fc5c
David <j.david.prado1716@gmail.com>
2023-05-03 10:04:35 -0500

    generando JWT en el servicio

--- api/routes/auth.router.js
+++ api/routes/auth.router.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
+const { config } = require('../config/config');
 
 router.post(
   '/login',
-  // indicamos que la estrategia es local y que no queremos manejar sesiones.
+  // verificamos el email y el password
   passport.authenticate('local', { session: false }),
   //si todo sale bien pasa a este middleware
   async (req, res, next) => {
     try {
-      res.json(req.user);
+      // se obtiene el usuario
+      const user = req.user;
+      // se crea un payload con user
+      const payload = {
+        sub: user.id,
+        role: user.role,
+      };
+      // lo firmamos
+      const token = jwt.sign(payload, config.jwtSecret);
+      //enviamos el usuario y el token
+      res.json({ user, token });
     } catch (error) {
       next(error);
     }
